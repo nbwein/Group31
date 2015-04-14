51082b68afbd3199ecdf2c31988a86aff774600c
Nicholas Wein <Nick@hnt.2039.wlanwireless-pennnet.upenn.edu>
2015-04-14 18:30:21 -0400

    fixed dashboard

--- app/src/main/java/upenn/edu/playscription/DashboardActivity.java
+++ app/src/main/java/upenn/edu/playscription/DashboardActivity.java
@@ -36,7 +36,7 @@ public class DashboardActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dashboard_layout);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
